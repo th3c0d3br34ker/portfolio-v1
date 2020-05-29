b103c489df6791f20c0586aae5bb6cf63681733d
Jainam Desai <jainamvipul.desai2019@vitstudent.ac.in>
2020-05-29 00:54:18 +0000

    Minor changes

---
+++ precache-manifest.271074939b5d0e2e3aab40f621da67be.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277cc816054e4d1a53b9abee691acb88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66e6e97f5a44469db6ac",
+    "url": "/static/css/main.bf51da34.chunk.css"
+  },
+  {
+    "revision": "073808952243aa7af3ce",
+    "url": "/static/js/2.fd5806fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd5806fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e6e97f5a44469db6ac",
+    "url": "/static/js/main.819d82f3.chunk.js"
+  },
+  {
+    "revision": "27686dbbb9be911269ef",
+    "url": "/static/js/runtime-main.b4f9c2a5.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
