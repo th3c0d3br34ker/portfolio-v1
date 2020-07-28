9c782bf2669afe8d3f27a91a16202f1dd762aa26
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2020-07-29 01:16:23 +0530

    Added black theme

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
-        <p className="footer-text">{emoji("Made with ❤️ by Jainam Desai")}</p>
+        <p className="footer-text">{emoji("Made with 🤍 by Jainam Desai")}</p>
       </div>
     </Fade>
   );
